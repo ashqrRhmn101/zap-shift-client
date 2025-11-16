ef419d9756d43c4cbaea19da3ef2e5058711bb18
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-11-16 12:30:34 +0600

    setUp zap-shift-client project

---
+++ src/Layout/RootLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
+
+const RootLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <div className="flex-1">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default RootLayout;
