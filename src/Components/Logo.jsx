ddfe0496d71298e58a3e70ed905afb381f05c182
ashqrRhmm101 <ashiqurrahman3529@gmail.com>
2025-11-16 13:09:38 +0600

    Navbar & Footer some modified

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from "../assets/logo.png"
+
+const Logo = () => {
+    return (
+        <div className='flex justify-center items-center'>
+            <img src={logo} alt="" />
+            <h3 className='text-2xl font-bold -ms-2.5'>ZapShift</h3>
+        </div>
+    );
+};
+
+export default Logo;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import Logo from "./Logo";
+import { NavLink } from "react-router";
 
 const NavBar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="/services">Services</NavLink>
+      </li>
+      <li>
+        <NavLink to="/coverage">Coverage</NavLink>
+      </li>
+      <li>
+        <NavLink to="/aboutUs">About Us</NavLink>
+      </li>
+      <li>
+        <NavLink to="/pricing">Pricing</NavLink>
+      </li>
+      <li>
+        <NavLink to="/blog">Blog</NavLink>
+      </li>
+      <li>
+        <NavLink to="/contact">Contact</NavLink>
+      </li>
+    </>
+  );
   return (
     <div>
       <div className="navbar bg-base-100 shadow-sm">
@@ -27,52 +51,22 @@ const NavBar = () => {
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {links}
+             
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">
+            <Logo />
+          </a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {links}
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <a className="btn">Sign In</a>
+          <a className="btn bg-primary">Sign Up</a>
         </div>
       </div>
     </div>
